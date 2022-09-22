f4602a7455f0991b84fc2b36d4d6af9cf80529e4
Scott Vance <scott@iyio.io>
2022-09-22 10:18:39 -0400

    - updated import link

--- scripts/open-validation-links.sh
+++ scripts/open-validation-links.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 cd "$(dirname "$0")"
-source config.sh
+source ./config.sh
 
 open "https://$NX_AWS_REGION.console.aws.amazon.com/acm/home?region=$NX_AWS_REGION#/certificates/list"
