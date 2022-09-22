58e90b06ba763bdc45e200d93b409ab58a40468e
Scott Vance <scott@iyio.io>
2022-09-22 08:50:20 -0400

    - Added message to create pipeline command

--- scripts/create-pipeline-for-branch.sh
+++ scripts/create-pipeline-for-branch.sh
@@ -2,6 +2,12 @@
 set -e
 cd "$(dirname "$0")/.."
 
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+echo "Creating pipeline for $BRANCH"
+echo "Press Ctrl+C to cancel"
+sleep 5
+
 npm run install-all
 
-scripts/nx-run-with-env.sh "branch-$(git rev-parse --abbrev-ref HEAD)" 'cdk:deploy' '1'
+scripts/nx-run-with-env.sh "branch-$BRANCH" 'cdk:deploy' '1'
